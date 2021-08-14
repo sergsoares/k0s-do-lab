33f41419540e3a0878886a4fa2f3e2d020229427
sergsoares <acad.sergio@gmail.com>
2021-08-13 22:34:40 -0300

    Create start.sh

--- README.md
+++ README.md
@@ -18,3 +18,4 @@ bash delete.sh
 
 - https://imagineer.in/blog/squid-proxy-droplet/
 - https://docs.k0sproject.io/v1.21.3+k0s.0/install/
+- http://blog.aeciopires.com/instalando-o-k0s-distribuicao-kubernetes-all-inclusive/
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 
-doctl compute droplet-action power-on $(doctl compute droplet list --format ID --no-header --tag-name k0s) 
+doctl compute droplet-action power-on $(doctl compute droplet list --format ID --no-header --tag-name k0s)
